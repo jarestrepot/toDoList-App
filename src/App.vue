40470e92889bae9847f8c17f8137b2efe7c810d4
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2023-11-20 21:05:13 +0100

    Change de router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
-  // import Dashboard from './pages/Dashboard.vue';
-  // import Login from './pages/Login.vue';
+
 </script>
 
 <template>
-  <!-- <Dashboard />
-  <Login /> -->
-  <router-view>
-
-  </router-view>
+  <router-view />
 </template>
 
 <style scoped>
