7e1836e987091e97ffce50f67bb9531937313012
Valentina Alzate <valzate618@gmail.com>
2023-12-12 22:05:52 +0100

    Change path background login

--- src/modules/auth/layout/AuthLayout.vue
+++ src/modules/auth/layout/AuthLayout.vue
@@ -6,7 +6,7 @@
     emits: ['registerClicked'],
     data() {
       return {
-        src: 'public/login-background.webp',
+        src: '../assets/images/login-background.webp',
         alt: 'App task',
         classImage: 'object-cover w-full h-screen',
       }
