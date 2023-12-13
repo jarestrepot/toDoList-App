7b8e8b729d5831f010f50be588c6325a249e0f9c
Valentina Alzate <valzate618@gmail.com>
2023-12-13 16:57:31 +0100

    Change path img login-background

--- src/modules/auth/layout/AuthLayout.vue
+++ src/modules/auth/layout/AuthLayout.vue
@@ -6,7 +6,7 @@
     emits: ['registerClicked'],
     data() {
       return {
-        src: '../assets/images/login-background.webp',
+        src: '/src/modules/auth/assets/images/login-background.webp',
         alt: 'App task',
         classImage: 'object-cover w-full h-screen',
       }
